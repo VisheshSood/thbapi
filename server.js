b18e3dcfa9c569219781eaaaba715a34838ad326
Vishesh Sood <vishesh@innovativeglove.com>
2020-03-20 18:03:09 +0700

    no message

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ var listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.get("/all/", async function(req, res) {
   counter++;
